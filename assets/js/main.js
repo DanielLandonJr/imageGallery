518f78efd00bee9b7724f3e47c1ad0370c97b81f
Daniel C. Landon Jr <daniel.landonjr@gmail.com>
2018-07-02 07:03:53 -0400

    Minor Tweaks, Better Comments.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 const ImageGallery = (() => {
+  // get large image
   const current = document.querySelector('#current');
+  // get array of small images
   const imgs = document.querySelectorAll('.imgs img');
+  // if we use opacity on small images this is the value
   const opacity = 0.6;
 
   const imgClick = (event) => {
-    // reset opacity value of all images to 1
-    imgs.forEach(img => (img.style.opacity = 1));
+    // reset opacity value of all images to 1 or remove the blur effect
+    // imgs.forEach(img => (img.style.opacity = 1));
+    imgs.forEach(img => (img.classList.remove('selectedImage')));
 
     // change large image to clicked image
     current.src = event.target.src;
@@ -13,29 +17,30 @@ const ImageGallery = (() => {
     // add fade animation to large image
     current.classList.add('fade-in');
 
-    // make sure fade in animation class is not on the main image
+    // make sure fade in animation class is removed from the main image
     // if we dont do this then the fade in animation will only happen one time
     // there are other ways of doing this but hey why not use a time event, remove after 1/4 sec
     setTimeout(() => {
       current.classList.remove('fade-in');
     }, 250);
 
-    // change opacity of clicked image
-    event.target.style.opacity = opacity;
+    // change the opacity or blur it out
+    // event.target.style.opacity = opacity;
+    event.target.classList.add('selectedImage');
   };
 
   // public methods
   return {
     init: () => {
-      // set the opacity of the first image so that it appears faded when the page loads
-      imgs[0].style.opacity = opacity;
+      // set the first small image to appear as if it was clicked...since it is the large image displayed when page loads
+      // change the opacity or blur it out
+      // imgs[0].style.opacity = opacity;
+      imgs[0].classList.add('selectedImage');
 
       imgs.forEach(img => {
-        // add click event to all images in the array
+        // add click event to all images in the small image array
         img.addEventListener('click', imgClick);
       });
-
-      // imgClick();
     }
   };
 })();
